82e0f77176a4037dcfb39850b670b61629511a78
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-08-06 10:03:24 -0300

    Atualizando Cliente com PUT

--- api/src/main/java/com/talissonmelo/review/resource/ClientResource.java
+++ api/src/main/java/com/talissonmelo/review/resource/ClientResource.java
@@ -2,6 +2,7 @@ package com.talissonmelo.review.resource;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -48,5 +50,13 @@ public class ClientResource {
 	public ResponseEntity<Void> delete(@PathVariable Long clientId){
 		service.deleteByClientId(clientId);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{clientId}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void update(@PathVariable Long clientId, @RequestBody Client clientUpdate) {
+		Client client = service.findById(clientId);
+		BeanUtils.copyProperties(clientUpdate, client, "id", "register");
+		repository.save(clientUpdate);
 	}
 }
