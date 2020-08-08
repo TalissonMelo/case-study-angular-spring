245804abf82024d779181c2ac2b2071b28b33509
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-08-08 11:14:54 -0300

    Toggle sidebar

--- api-ui/src/app/app.component.ts
+++ api-ui/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import jQuery from 'jquery'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'api-ui';
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    (function ($) {
+      var path = window.location.href;
+      $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
+        if (this.href === path) {
+          $(this).addClass("active");
+        }
+      });
+
+      $("#sidebarToggle").on("click", function (e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+      });
+    })(jQuery);
+
+  }
 }
