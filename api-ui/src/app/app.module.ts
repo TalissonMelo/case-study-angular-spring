a2be52b37d879d8a117b8faec3b3608d5ffc7300
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-08-09 11:29:26 -0300

    Criando Arquivo de Rotas

--- api-ui/src/app/app.module.ts
+++ api-ui/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { TemplateModule } from './template/template.module';
@@ -12,7 +13,8 @@ import { HomeComponent } from './home/home.component'
   ],
   imports: [
     BrowserModule,
-    TemplateModule
+    TemplateModule,
+    AppRoutingModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
