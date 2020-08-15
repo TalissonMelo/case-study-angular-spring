241d2ee8524a37c02c8da6196ce7226269994a39
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-08-15 14:38:37 -0300

    Método de pesquisa de serviços

--- api/src/main/java/com/talissonmelo/review/repository/OrderServiceRepository.java
+++ api/src/main/java/com/talissonmelo/review/repository/OrderServiceRepository.java
@@ -1,9 +1,17 @@
 package com.talissonmelo.review.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.talissonmelo.review.model.OrderService;
 
 public interface OrderServiceRepository extends JpaRepository<OrderService, Long>{
 
+	@Query(" select s from OrderService s join s.client c"
+			+ " where upper( c.name ) like upper( :name ) and MONTH(s.register) =:month")
+	List<OrderService> findByNameClientAndMonth(
+			@Param("name") String name,@Param("month") Integer month);
 }
--- api/src/main/java/com/talissonmelo/review/resource/OrderServiceResource.java
+++ api/src/main/java/com/talissonmelo/review/resource/OrderServiceResource.java
@@ -1,11 +1,15 @@
 package com.talissonmelo.review.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,16 +21,24 @@ import com.talissonmelo.review.service.OrderServiceService;
 @RestController
 @RequestMapping(value = "/order-services")
 public class OrderServiceResource {
-	
+
 	@Autowired
 	private OrderServiceService service;
-	
+
 	@Autowired
 	private OrderServiceRepository repository;
 
+	@GetMapping
+	public List<OrderService> findAll(
+			@RequestParam(value = "name", required = false, defaultValue = "" ) String name,
+			@RequestParam(value = "month", required = false) Integer month) {
+		
+		return repository.findByNameClientAndMonth("%" + name + "%", month);
+	}
+
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	public ResponseEntity<OrderService> insert(@RequestBody OrderServiceDTO dto){
+	public ResponseEntity<OrderService> insert(@RequestBody OrderServiceDTO dto) {
 		OrderService orderService = service.toDto(dto);
 		orderService = service.insert(orderService);
 		return ResponseEntity.ok().body(orderService);
