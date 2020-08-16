d14878bf12c3adf0403ed9dd6a6ec20ada52383d
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-08-16 15:44:43 -0300

    Cadastrando Ordem de Serviço com POST

--- api-ui/src/app/order-service/order-service-form/order-service-form.component.ts
+++ api-ui/src/app/order-service/order-service-form/order-service-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Client } from 'src/app/client/model/Client';
 import { ClientService } from 'src/app/client.service';
 import { OrderService } from '../order.service'
+import { OrderServiceService } from 'src/app/order-service.service';
 
 @Component({
   selector: 'app-order-service-form',
@@ -13,7 +14,8 @@ export class OrderServiceFormComponent implements OnInit {
   clients: Client[] = []
   orderService: OrderService
 
-  constructor(private cleintService: ClientService) { }
+  constructor(private cleintService: ClientService,
+    private service: OrderServiceService) { }
 
   ngOnInit(): void {
     this.cleintService.findAllClient().subscribe(response => this.clients = response);
@@ -21,6 +23,6 @@ export class OrderServiceFormComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.orderService)
+    this.service.insertOrderService(this.orderService).subscribe(response => console.log(response));
   }
 }
--- api/src/main/java/com/talissonmelo/review/resource/OrderServiceResource.java
+++ api/src/main/java/com/talissonmelo/review/resource/OrderServiceResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ import com.talissonmelo.review.service.OrderServiceService;
 
 @RestController
 @RequestMapping(value = "/order-services")
+@CrossOrigin(value = "http://localhost:4200")
 public class OrderServiceResource {
 
 	@Autowired
