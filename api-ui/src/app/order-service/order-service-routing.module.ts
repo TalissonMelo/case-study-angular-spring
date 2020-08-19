28c001c7641496225e9e7d8002621ccdbf98b7ee
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-08-19 10:10:27 -0300

    Trabalhando com rotas filhas no modulo de Serviço

--- api-ui/src/app/order-service/order-service-routing.module.ts
+++ api-ui/src/app/order-service/order-service-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OrderServiceFormComponent } from './order-service-form/order-service-form.component';
 import { OrderServiceListComponent } from './order-service-list/order-service-list.component';
+import { LayoutComponent } from '../layout/layout.component';
 
 const routes: Routes = [
-  {path: 'order-service-form' , component : OrderServiceFormComponent},
-  {path: 'order-service-list', component: OrderServiceListComponent}
+  {
+    path: 'order-service', component: LayoutComponent, children: [
+      { path: 'form', component: OrderServiceFormComponent },
+      { path: 'list', component: OrderServiceListComponent },
+      {path: '', redirectTo: '/order-service/list', pathMatch: 'full'}
+    ]
+  }
+
 ];
 
 @NgModule({
