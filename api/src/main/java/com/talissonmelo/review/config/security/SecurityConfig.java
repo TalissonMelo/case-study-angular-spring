7d095e21c6531f21cc0c7306bef70b009a0418a2
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-08-23 10:09:25 -0300

    Configurando o Spring Security

--- api/src/main/java/com/talissonmelo/review/config/security/SecurityConfig.java
+++ api/src/main/java/com/talissonmelo/review/config/security/SecurityConfig.java
@@ -1,13 +1,33 @@
 package com.talissonmelo.review.config.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
-@EnableResourceServer
-@EnableAuthorizationServer
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("Talisson").password("123").roles("USER");
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.cors()
+		.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+	}
 }
