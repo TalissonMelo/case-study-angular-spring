10623ae2d388da226281d39b2428f9a0c797239a
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-08-23 09:59:40 -0300

    Fluxo do OAuth2 e adicionando as dependências e testando a configuração padrão

---
+++ api/src/main/java/com/talissonmelo/review/config/security/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.talissonmelo.review.config.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableResourceServer
+@EnableAuthorizationServer
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+}
