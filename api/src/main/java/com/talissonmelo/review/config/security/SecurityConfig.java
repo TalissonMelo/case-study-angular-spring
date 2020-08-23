1481ea5978d4542fb803603c50eb5e287b9fff35
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-08-23 10:48:09 -0300

    Criando Bean Password

--- api/src/main/java/com/talissonmelo/review/config/security/SecurityConfig.java
+++ api/src/main/java/com/talissonmelo/review/config/security/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -29,5 +31,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
